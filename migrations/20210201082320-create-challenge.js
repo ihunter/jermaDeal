bfbf4bc02778aec31ee736219a3756d7b6caa492
ihunter <ijh.hunter@gmail.com>
2021-02-01 03:31:10 -0500

    create Challenge model

---
+++ migrations/20210201082320-create-challenge.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Challenges', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      worth: {
+        type: Sequelize.DOUBLE
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      positive: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      negative: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Challenges');
+  }
+};
---
+++ models/challenge.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Challenge extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Challenge.init({
+    userId: DataTypes.STRING,
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    worth: DataTypes.DOUBLE,
+    type: DataTypes.STRING,
+    username: DataTypes.STRING,
+    positive: DataTypes.INTEGER,
+    negative: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Challenge',
+  });
+  return Challenge;
+};
